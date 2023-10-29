b825f1bc92aa4fcc38d5f33d7f305ec6f66ffe4a
Bryan Azofeifa Agüero <bryan18is@hotmail.com>
2023-10-29 11:55:52 -0600

    Set port

--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe);
-  await app.listen(+process.env.PORT);
+  const port: number = +process.env.PORT || 3000;
+  await app.listen(port);
 }
 bootstrap();
