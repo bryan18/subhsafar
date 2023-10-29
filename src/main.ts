c6fd255c0edf718767d16a50040662b84cebfe41
Bryan Azofeifa Agüero <bryan18is@hotmail.com>
2023-10-29 12:22:52 -0600

    Fix port 2

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe);
   const port: number = +process.env.PORT || 3000;
-  await app.listen(port, () => {
-    console.log('Hola estamos en el puerto:',port);
-    
-  });
+  await app.listen(port);
 }
 bootstrap();
