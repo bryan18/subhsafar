c32930ff4766ddaf98d2ce1cfa09f4cca11691f2
Bryan Azofeifa Agüero <bryan18is@hotmail.com>
2023-10-29 12:06:30 -0600

    Fix port

--- src/main.ts
+++ src/main.ts
@@ -8,6 +8,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe);
   const port: number = +process.env.PORT || 3000;
-  await app.listen(port);
+  await app.listen(port, () => {
+    console.log('Hola estamos en el puerto:',port);
+    
+  });
 }
 bootstrap();
